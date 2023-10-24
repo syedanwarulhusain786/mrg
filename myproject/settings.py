e8cd27c46c1faab219759271d83d9694761cf471
syedanwarulhusain786 <syedanwarulhusain68@gmail.com>
2023-10-25 01:07:43 +0530

    first commit1

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,6 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+print(STATIC_ROOT)
 STATICFILES_DIRS=[
         os.path.join(BASE_DIR,'static'),
         ]
