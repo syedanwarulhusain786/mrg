1fa35cc10023ccfa8db4eca1e674a61d38187477
syedanwarulhusain786 <syedanwarulhusain68@gmail.com>
2023-10-24 21:56:18 +0530

    final code

--- testapp/urls.py
+++ testapp/urls.py
@@ -19,15 +19,15 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.index, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('home/', views.test, name='home'),
+    path('home/', views.home, name='home'),
     path('download/', views.download, name='download'),
     path('video/', views.video, name='video'),
     path('faq/', views.faq, name='faq'),
     
     
+    path('logout/', views.logoutUser, name='logout'),
     
-    
-    # path('sitefinder/', views.test, name='sitefinder'),
+    path('setfinder/', views.setfinder, name='setfinder'),
     path('upload_file/', views.upload_file, name='upload_file'),
     
     path('video_cards/', views.video_cards, name='video_cards'),
@@ -35,6 +35,9 @@ urlpatterns = [
     path('success/',views.success,name='success'),
     path('cancel/',views.cancel,name='cancel'),
     path('webhook/stripe',views.my_webhook_view,name='webhook-stripe'),
+    
+    path('download/',views.handle_custom_download_submit,name='handle_custom_download_submit'),
+    
     
 
 
--- testapp/views.py
+++ testapp/views.py
@@ -15,15 +15,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+import xml.etree.ElementTree as ET
 
 
 # from accounts.serializers import *
 import time
-    
+from django.contrib.auth.decorators import login_required
+import stripe
+from django.conf import settings
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Video
 from django.http import JsonResponse
-from django.template.loader import render_to_string
 import datetime
 from django.db.models import Q
 from datetime import datetime, timedelta
@@ -31,21 +34,22 @@ from bs4 import BeautifulSoup
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib import messages
-# Create your views here.
-# views.py
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.views import LogoutView
 from django.contrib.auth.models import User
 import re
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+
+#stripe listen --forward-to localhost:8000/webhook/stripe
 def index(request):
     if request.method == 'POST':
         email=request.POST['email']
         password=request.POST['password']
         
         user = authenticate(request, username=email, password=password)
-        print(user)
         
         if user is not None:
             login(request, user)
@@ -60,8 +64,9 @@ def signup(request):
     return render(request, 'signup.html')
 
 
-
-
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 
@@ -221,14 +226,13 @@ def calculation(date2_str,date1_str):
     # Calculate the number of weeks
     weeks = date_difference / 7
     return weeks
-def test(request):
-
-    return render(request, 'home.html')
+@login_required(login_url='login')
 def download(request):
     files = DownloadItem.objects.all()  # Retrieve all videos from your database
     
     context = {'files': files,"user":request.user}
     return render(request, 'download.html',context)
+@login_required(login_url='login')
 def video(request):
     videos = Video_Ea.objects.all()  # Retrieve all videos from your database
     
@@ -236,6 +240,7 @@ def video(request):
     return render(request, 'video.html',context)
 
 import json
+
 def upload_file(request):
     merged_data = [] 
     file_name=''
@@ -265,7 +270,7 @@ def upload_file(request):
         return JsonResponse({"page":merged_data})
 
 
-
+@login_required(login_url='login')
 def video_cards(request):
     videos = Video.objects.all()  # Retrieve all videos from your database
     
@@ -273,7 +278,7 @@ def video_cards(request):
     return render(request, 'library.html', context)
 
 
-
+@login_required(login_url='login')
 def faq(request):
     Faqs = Faq.objects.all()  # Retrieve all videos from your database
     print()
@@ -281,9 +286,6 @@ def faq(request):
     return render(request, 'faq.html',context)
 
 
-import stripe
-from django.conf import settings
-from django.urls import reverse
 stripe.api_key=settings.STRIPE_SECRET_KEY
 endpoint_secret=settings.STRIPE_WEBHOOK_SECRET
 def CreateCheckoutSessionView(request):
@@ -359,7 +361,6 @@ def cancel(request):
        'payment_status' :'cancel'
     }
     return render(request,'cancel.html',context)
-from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 
@@ -406,128 +407,173 @@ def register_user(session):
         country = session['metadata']['bcountry'],
     )
     pay.save()
-      
-
-
-
-
-
-# class MergeCSV(APIView):
-#     parser_class = (FileUploadParser,)
-#     parser_class = (FileUploadParser,)
-
-#     def post(self, request):
-        
-#         file1 = request.data.get('file1')
-#         file2 = request.data.get('file2')
-#         if file1.name.find('forward'):
-#             forward=file1.read()
-#             backward=file2.read()
-#         elif file2.name.find('forward'):
-#             forward=file2.read() 
-#             backward=file1.read()
-#         else:
-#             return Response({'error': 'Atleast one XML files should be forward.'}, status=status.HTTP_400_BAD_REQUEST)
-#         # Check if both files are provided
-#         if not file1 or not file2:
-#             return Response({'error': 'Both XML files are required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-#         dt = get_date(forward,backward)
-#         print(dt)
-#         # Read and merge XML files
-#         # merged_data = self.merge_xml(forward, backward)
-
-#         # Serialize the merged data
-#         # serializer = DataEntrySerializer(data=merged_data, many=True)
-#         # if serializer.is_valid():
-#         #     serializer.save()
-#         #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(dt, status=status.HTTP_400_BAD_REQUEST)
-
-#     def merge_xml(self, file1, file2):
-#         # Implement your CSV merging logic here
-#         # You can use the csv module to read and merge the data from the two files
-#         soup1 = BeautifulSoup(file1, 'xml')
-#         soup2 = BeautifulSoup(file2, 'xml')
-
-#         # Implement your merging logic here
-        
-        
-#         # For demonstration purposes, we'll just concatenate the XML strings
-#         merged_xml_data = get_rows_back(soup2)
-#         print(len(merged_xml_data))
-#         time.sleep(10)
-        
-#         merged_xml_data = get_rows_forwad(soup1,merged_xml_data)
-#         time.sleep(10)
-#         print(len(merged_xml_data))
-        
-#         # Example:
-#         # Read file1 and file2 and merge the data into merged_data
-
-#         return merged_xml_data
-
-# def get_rows_back(soup):
-#     data={}
-#     back_head=soup.find('Row').find_all("ss:Cell")
-#     back_pass=back_head[0].text
-#     back_result=back_head[1].text
-#     back_profit=back_head[2].text
-#     back_r_f=back_head[5].text
-#     back_e_dd_per=back_head[8].text
-#     back_trades=back_head[9].text
-#     for d in soup.find_all('Row')[1:]:
-#         dt = d.find_all("Cell")
-        
-#         data[dt[0].text]={
-#             f"back_{back_result}":dt[1].text,
-#             f"back_{back_profit}":dt[2].text,
-#             f"back_{back_r_f}":dt[5].text,
-#             f"back_{back_e_dd_per}":dt[8].text,
-#             f"back_{back_trades}":dt[9].text,
-#         }
+@login_required(login_url='login')    
+def setfinder(request):
     
-#     return data
-
-# def get_rows_forwad(soup,res):
-#     data={}
-#     forward_head=soup.find('Row').find_all("ss:Cell")
-#     forward_pass=forward_head[0].text
-#     forward_result=forward_head[1].text
-#     forward_profit=forward_head[3].text
-#     forward_r_f=forward_head[6].text
-#     forward_e_dd_per=forward_head[9].text
-#     forward_trades=forward_head[10].text
-#     for d in soup.find_all('Row')[1:]:
-#         dt = d.find_all("Cell")
-#         data[dt[0].text]={**res[dt[0].text], **{f"forward_{forward_result}":dt[1].text,f"forward_{forward_profit}":dt[2].text,f"forward_{forward_r_f}":dt[5].text,f"forward_{forward_e_dd_per}":dt[8].text,f"forward_{forward_trades}":dt[9].text,}}
-#         print(data[dt[0].text])
-#     return data
+    return render(request, 'setfinder.html')
+@login_required(login_url='login')
+def home(request):
+    
+    return render(request, 'home.html')
 
 
-# def get_date(forward, backward):
-#     # Implement your CSV merging logic here
-#     # You can use the csv module to read and merge the data from the two files
-#     soup1 = BeautifulSoup(forward, 'xml')
-#     soup2 = BeautifulSoup(backward, 'xml')
-#     forward_title=soup1.find('Title').text.split('M1')[-1].strip()
-#     forward_date=forward_title.split('-')
-#     forward_date_start=forward_date[0]
-#     forward_date_end=forward_date[1]
-#     backward_title=soup2.find('Title').text.split('M1')[-1].strip()
-#     backward_date=backward_title.split('-')
-#     backward_date_start=backward_date[0]
-#     backward_date_end=backward_date[0]
-#     if forward_title==backward_title:
-#         dt=[{
-#             'forward_date_start':forward_date_start,
-#             'forward_date_end':forward_date_end,
-#             'backward_date_start':backward_date_start,
-#             'backward_date_end':backward_date_end,
-#         }]
-#         print(dt)
-#         return Response(dt, status=status.HTTP_200_OK)
-#     else:
-#         return  Response(False, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+def get_rows_forward(xmlDataForward, back, diff, file_name, deposit, drawdown):
+    data = []
+
+    data = []
+    for row in xmlDataForward.find_all('Row'):
+        rowData = []
+        for cell in row.find_all('Cell'):
+            cellData = cell.find('Data')
+            rowData.append(cellData.text if cellData is not None else '')
+
+        data.append(rowData)
+        
+
+    forward_head = data[0]
+    forward_pass = forward_head[0]
+    forward_result = forward_head[1].replace(' ', '_')
+    forward_profit = forward_head[3].replace(' ', '_')
+    forward_r_f = forward_head[6].replace(' ', '_')
+    forward_e_dd_per = forward_head[9].replace(' ', '_').replace('%', '')
+    forward_trades = forward_head[10].replace(' ', '_')
+    data.pop(0)
+
+    data_dict = {}
+
+    for dt in data:
+
+        forward_pass = dt[0]
+        print(dt)
+        print(back[forward_pass]['back_Profit'])
+        print(diff)
+        profit_match = 0  # Default value
+
+        if back[forward_pass]['back_Profit'] != 0:
+            profit_match = round((float(dt[2]) / (float(back[forward_pass]['back_Profit']) / diff) * 100), 2)
+
+        total_profit = round(float(dt[2]) + float(back[forward_pass]['back_Profit']), 2)
+        max_original_dd = round(float(back[forward_pass]['back_Equity_DD_d']) * (float(float(deposit)) / 100), 2)
+        lot_multiple = 0
+
+        if back[forward_pass]['back_Equity_DD_d'] != 0:
+            lot_multiple = round(drawdown / (round(float(back[forward_pass]['back_Equity_DD_d'])* (float(deposit) / 100), 2)), 2)
+
+        estimated_total_dd = 0
+        estimated_profit = 0
+
+        if back[forward_pass]['back_Equity_DD_d'] != 0:
+            estimated_total_dd = round(round(drawdown / (round(float(back[forward_pass]['back_Equity_DD_d']) * (float(deposit) / 100), 2)) * round(float(back[forward_pass]['back_Equity_DD_d']) * (float(deposit) / 100), 2)), 2)
+            estimated_profit = round((dt[2] + float(back[forward_pass]['back_Profit'])) * (round(drawdown / (round(float(back[forward_pass]['back_Equity_DD_d']) * (float(deposit) / 100), 2)), 2)), 2)
+
+        max_dd = round(max(float(back[forward_pass]['back_Equity_DD_d']), dt[10]) * (float(deposit) / 100), 2)
+
+        data_dict[forward_pass] = {
+            "file_name": file_name,
+            "pass": dt[0],
+            "forward_result": dt[1],
+            "forward_profit": dt[2],
+            "forward_r_f": dt[6],
+            "forward_e_dd_per": dt[9],
+            "forward_trades": dt[10],
+            "back_Result": back[forward_pass]['back_Result'],
+            "back_Profit": back[forward_pass]['back_Profit'],
+            "back_Recovery_Factor": back[forward_pass]['back_Recovery_Factor'],
+            "back_Equity_DD_d": back[forward_pass]['back_Equity_DD_d'],
+            "back_Trades": back[forward_pass]['back_Trades'],
+            "profit_match": profit_match,
+            "total_profit": total_profit,
+            "max_original_dd": max_original_dd,
+            'lot_Multiple': lot_multiple,
+            "Estimated_Total_DD": estimated_total_dd,
+            "Estimated_Profit": estimated_profit,
+            "Max_DD": max_dd,
+        }
+
+    return data_dict
+
+def calculation(date2_str, date1_str):
+    # Convert the date strings to datetime objects
+    date1 = datetime.strptime(date1_str, '%Y-%m-%d')
+    date2 = datetime.strptime(date2_str, '%Y-%m-%d')
+
+    # Calculate the difference in days between the two dates
+    date_difference = (date2 - date1).days
+
+    # Calculate the number of weeks
+    weeks = date_difference / 7
+    return weeks
+
+
+
+
+
+
+
+
+
+
+@login_required(login_url='login')
+@csrf_exempt
+def handle_custom_download_submit(request):
+    # Verify nonce if needed
+        # Handle form data and files here
+    row_id = request.POST.get('row_id')
+    
+    if 'fileforward' in request.FILES and 'fileset' in request.FILES:
+        fileforward = request.FILES['fileforward']
+        filenameset = request.FILES['fileset']
+        
+        # Handle file uploads here
+
+        # Check if both files were uploaded successfully
+        if fileforward and filenameset:
+            xmlContentForward = fileforward.read()
+            Contentfilenameset = filenameset.read()
+            
+            # Parse XML data from the 'Forward' file
+            # Your XML parsing code goes here
+
+            data = {}  # Your data processing logic goes here
+            headers = []
+            is_first_row = True
+
+            soup = BeautifulSoup(xmlContentForward, 'lxml-xml')  # Use 'lxml-xml' parser for XML content
+
+            for row in soup.find_all('Row'):
+                rowData = []
+                passValue = None
+
+                for cell in row.find_all('Cell'):
+                    cellData = cell.find('Data')
+                    cellValue = cellData.text if cellData else ''
+
+                    if is_first_row:
+                        headers.append(cellValue.replace(' ', '').replace('_', '').lower())
+                    else:
+                        rowData.append(cellValue)
+
+                if not is_first_row:
+                    passValue = rowData[0]
+                    if passValue == row_id:
+                        data[passValue] = dict(zip(headers, rowData))
+
+                is_first_row = False
+                    # Prepare response data
+            response = data[row_id]
+
+            # Send response as JSON
+            return JsonResponse(response)
+
+        else:
+            return JsonResponse({'message': 'Error uploading .set file files.'})
+
+    else:
+        return JsonResponse({'message': 'File fields not found.'})
+
+
+
+
